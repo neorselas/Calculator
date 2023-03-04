9b0479af8f9e8a7fa28c7151bf48dedf1bef98b4
neorselas <126072709+neorselas@users.noreply.github.com>
2023-03-04 15:22:36 +0800

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,12 +1,29 @@
-function func1(){
-    let btn1 =document.getElementById("one").value;
-    let btn2 =document.getElementById("two").value;
-    let operator = document.getElementById("add").value;
+// function func1(){
+//     let btn1 =document.getElementById("one").value;
+//     // let btn2 =document.getElementById("two").value;
+//     let operator = document.getElementById("add").value;
 
-    let input = document.getElementById("cal_field");
-    input.value = btn1;
+//     let input = document.getElementById("cal_field");
+//     input.value = btn1;
+// }
+
+let result = document.getElementById('calc_field');
+let Calculate=(number)=>{
+    result.value+=number;
 }
 
+let Result=()=>{
+      {
+         try
+         {
+            calc_field.value = eval(calc_field.value);
+         }
+         catch(err)
+         {
+            alert("Invalid")
+         }
+      }
+    }
 
 
 //console.log(btn1);
