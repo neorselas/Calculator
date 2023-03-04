37b42f1242100649405703ac19478d320eb3ef94
neorselas <126072709+neorselas@users.noreply.github.com>
2023-03-04 12:52:13 +0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function func1(){
+    let btn1 =document.getElementById("one").value;
+    let btn2 =document.getElementById("two").value;
+    let operator = document.getElementById("add").value;
+
+    let input = document.getElementById("cal_field");
+    input.value = btn1;
+}
+
+
+
+//console.log(btn1);
---
+++ index.js
@@ -0,0 +1,20 @@
+let variable1 = document.getElementById("demo");
+variable1.innerHTML = "wooorrrddddsss";
+
+//innerHTML - <b> wooordds </b>
+//innerText - <p> test/string
+//textContent - <p> -text/string
+//value = input fields 
+
+
+//testing purposes
+document.write(5+6);
+
+//display data
+alert(5+6);
+alert("Hello World");
+
+//console.log
+console.log(variable1);
+
+//javascript events
